07e4ebdd08b3ee95d8405d7e9d6f1745ca623954
Tolulope <lanre.toluwa@gmail.com>
2020-11-02 21:35:59 +0100

    added answerbase models and submission

--- survey/models.py
+++ survey/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 from .subroutines import random_id
 from .fields import OrderField
@@ -44,3 +45,58 @@ class Question(models.Model):
 
     def __str__(self):
         return "Question %s" % self.order
+
+
+class Submission(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="submissions")
+    submitted_by = models.ForeignKey(SurveyUser, on_delete=models.SET_NULL, null=True, blank=True, related_name="submissions")
+    order = OrderField(blank=True, for_fields=["survey"])
+
+    date_submitted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Submission %s on %s" % (self.order, self.survey.title)
+
+
+class AnswerBase(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="%(class)s")
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE, related_name="%(class)s")
+
+    class Meta:
+        abstract = True
+
+
+class ShortAnswer(AnswerBase):
+    answer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.answer if (len(self.answer) <= 20) else "%s..." % self.answer[0:20]
+
+
+class ParagraphAnswer(AnswerBase):
+    answer = models.TextField()
+
+    def __str__(self):
+        return "%s..." % self.answer[0:20]
+
+
+class DateAnswer(AnswerBase):
+    answer = models.DateField()
+
+    def __str__(self):
+        return self.answer
+
+
+class TimeAnswer(AnswerBase):
+    answer = models.TimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.answer
+
+
+class DateTimeAnswer(AnswerBase):
+    answer = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.answer
+
