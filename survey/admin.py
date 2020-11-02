ec3b4320e630539d47747c98b5e1064335c06faf
Tolulope <lanre.toluwa@gmail.com>
2020-11-02 21:53:41 +0100

    registered new models

--- survey/admin.py
+++ survey/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import SurveyUser, Survey, Question
+from .models import (SurveyUser, Survey, Question, ShortAnswer, ParagraphAnswer,
+    DateAnswer, TimeAnswer, DateTimeAnswer, Submission)
 
 
 @admin.register(SurveyUser)
@@ -16,4 +17,13 @@ class QuestionInline(admin.TabularInline):
 @admin.register(Survey)
 class SurveyAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+
+
+admin.site.register(Submission)
+
+admin.site.register(ShortAnswer)
+admin.site.register(ParagraphAnswer)
+admin.site.register(DateTimeAnswer)
+admin.site.register(DateAnswer)
+admin.site.register(TimeAnswer)
 
