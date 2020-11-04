b950573830099d0af338ce7bc0d9f099e1c55a33
Tolulope <lanre.toluwa@gmail.com>
2020-11-04 21:03:50 +0100

    added option feature to question as a model

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
+from django.urls import reverse
 
 from .subroutines import random_id
 from .fields import OrderField
@@ -46,6 +47,14 @@ class Question(models.Model):
 
     def __str__(self):
         return "Question %s" % self.order
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="options")
+    body = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.body
 
 
 class Submission(models.Model):
